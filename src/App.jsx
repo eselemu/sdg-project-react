7ba18fff9f86fcdf7f0237af3e7bda48a6a30f7a
Emilio Ibarra <0257569@up.edu.mx>
2023-11-29 22:32:55 -0600

    added preliminar stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Temporal from './components/Temporal';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import ForumMain from './components/forum/ForumMain';
+import NewsletterMain from './components/newsletter/NewsletterMain';
 
 import './components/NavBar.css';
 
 function App() {
+  const news = require('./news.json');
   return (
     <div className="App">
+      
       <NavBar />
 
       <Routes>
         <Route path = "/" element = {<Temporal />} />
         <Route path = "/signup" element = {<Temporal />} />
         <Route path = "/login" element = {<Temporal />} />
-        <Route path = "/newsletter" element = {<Temporal />} />
+        <Route path = "/newsletter" element = {<NewsletterMain news={news} />} />
         <Route path = "/forum" element = {<ForumMain />} />
         <Route path = "/videogame" element = {<Temporal />} />
       </Routes>
